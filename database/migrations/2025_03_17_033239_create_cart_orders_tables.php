36edec8ca963d6108077cb9e62ce679f87d065f6
setthi-pheap <kingofsatthi@gmail.com>
2025-03-20 00:37:24 +0700

    new code

--- database/migrations/2025_03_17_033239_create_cart_orders_tables.php
+++ database/migrations/2025_03_17_033239_create_cart_orders_tables.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('cart_orders_tables', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_price', 8, 2);
             $table->uuid('product_id');
             $table->integer('quantity');
             $table->timestamps();
